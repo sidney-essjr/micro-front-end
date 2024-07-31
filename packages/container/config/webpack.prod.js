a57e9ffa8a757300d4687ba22440cd6b191cdcf0
sidney.jr <sidney.e.s.s.jr@gmail.com>
2024-07-31 16:53:58 -0300

    config: added invalidations step to build

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -1,7 +1,7 @@
 const { merge } = require("webpack-merge");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const commonConfig = require("./webpack.common");
-const { dependencies } = require("../package.json");
+const packageJson = require("../package.json");
 
 const domain = process.env.PRODUCTION_DOMAIN;
 
@@ -17,7 +17,7 @@ const prodConfig = {
       remotes: {
         marketing: `marketing@${domain}/marketing/remoteEntry.js`,
       },
-      shared: dependencies,
+      shared: packageJson.dependencies,
     }),
   ],
 };
