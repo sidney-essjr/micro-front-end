08e761e8ed42b9d21ba28ab22a795a6c5a79a164
sidney.jr <sidney.e.s.s.jr@gmail.com>
2024-08-02 09:43:02 -0300

    config: action flow from marketing deploy

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -1,6 +1,6 @@
 const { merge } = require("webpack-merge");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
-const packageJson = require("../package.json");
+const { dependencies } = require("../package.json");
 const commonConfig = require("./webpack.common");
 
 const prodConfig = {
@@ -15,7 +15,7 @@ const prodConfig = {
       exposes: {
         "./MarktingApp": "./src/bootstrap",
       },
-      shared: packageJson.dependencies,
+      shared: dependencies,
     }),
   ],
 };
