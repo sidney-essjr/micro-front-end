ab788888b10e690ddbb166ef61212f5370f61a05
sidney.jr <sidney.e.s.s.jr@gmail.com>
2024-08-02 12:03:34 -0300

    feat: finish Header feature

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,6 +1,13 @@
 import MarketingApp from "./components/MarketingApp";
+import Header from "./components/Header";
+import { BrowserRouter } from "react-router-dom";
 import React from "react";
 
 export default () => {
-  return <MarketingApp />;
+  return (
+    <BrowserRouter>
+      <Header />
+      <MarketingApp />;
+    </BrowserRouter>
+  );
 };
