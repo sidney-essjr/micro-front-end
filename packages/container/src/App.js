c0c9c0a3b63943043e225f64813a0e748b1885d2
sidney.jr <sidney.e.s.s.jr@gmail.com>
2024-08-02 15:19:09 -0300

    fix: randonize production classnames

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,13 +1,20 @@
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
 import { BrowserRouter } from "react-router-dom";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core";
 import React from "react";
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
 
 export default () => {
   return (
     <BrowserRouter>
-      <Header />
-      <MarketingApp />;
+      <StylesProvider generateClassName={generateClassName}>
+        <Header />
+        <MarketingApp />;
+      </StylesProvider>
     </BrowserRouter>
   );
 };
--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,4 +1,4 @@
-import { StylesProvider } from "@material-ui/core";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core";
 import React from "react";
 import {
   BrowserRouter,
@@ -8,10 +8,15 @@ import {
 import Landing from "./components/Landing";
 import Pricing from "./components/Pricing";
 
+// Generate a unique class name for each CSS class
+const generateClassName = createGenerateClassName({
+  productionPrefix: "ma",
+});
+
 export default () => {
   return (
     <React.Fragment>
-      <StylesProvider>
+      <StylesProvider generateClassName={generateClassName}>
         <BrowserRouter>
           <Switch>
             <Route exac path="/pricing" component={Pricing} />
