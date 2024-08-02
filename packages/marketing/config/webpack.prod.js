12184737c69331420135e503e842eb78380d8529
sidney.jr <sidney.e.s.s.jr@gmail.com>
2024-08-02 11:24:21 -0300

    fix: config

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -1,6 +1,6 @@
 const { merge } = require("webpack-merge");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
-const packageJson = require("../package.json");
+const { dependencies } = require("../package.json");
 const commonConfig = require("./webpack.common");
 
 const prodConfig = {
@@ -16,7 +16,7 @@ const prodConfig = {
       exposes: {
         "./MarketingApp": "./src/bootstrap",
       },
-      shared: packageJson.dependencies,
+      shared: dependencies,
     }),
   ],
 };
